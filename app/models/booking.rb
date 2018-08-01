d34223249eac899f5b68ad596907a211a756e534
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-01 20:28:57 +0800

    work in process booking controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :parking_space
   belongs_to :car
-  validates :parking_space, :car :start_time, :end_time, :price, :status, presence: true
+  validates :parking_space, :car, :start_time, :end_time, :price, :status, presence: true
 end
