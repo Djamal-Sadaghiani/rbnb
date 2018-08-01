b50e95a61926a7cdb82d6d135d94c16f9c163326
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-01 15:39:11 +0800

    added routes for parking_space, users, bookings, cars + root to home

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :parkingspaces
+  get "parkingspaces/search", to: "parkingspaces#search", as: :search_parkingspace
+
+  resources :users do
+    resources :bookings, only [:new, :create, :show, :edit, :update]
+    resources :cars
+  end
+
 end
