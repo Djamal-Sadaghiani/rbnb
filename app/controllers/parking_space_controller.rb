610cd5671562df1b0c306ad21ff22121e9b7e4dd
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-01 16:58:17 +0800

    parking controller wip

--- app/controllers/parking_space_controller.rb
+++ app/controllers/parking_space_controller.rb
@@ -5,10 +5,22 @@ class ParkingSpaceController < ApplicationController
 
   def new
     @parking_space = ParkingSpace.new
+    @user = User.find(params[:user_id])
   end
 
   def create
+    @parking_space = ParkingSpace.new
+    @user = User.find(params[:user_id])
+    if @parking_space.save
+      redirect_to user_path(@user)
+    else
+      render new
+  end
 
+  private
+
+  def parking_space_params
+    params.require(:parking_space).permit(:)
   end
 
 end
