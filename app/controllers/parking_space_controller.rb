e22d5789aa8b68a4056a4957f1b969b3c2beb581
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-01 17:04:03 +0800

    parking controller wip 2

--- app/controllers/parking_space_controller.rb
+++ app/controllers/parking_space_controller.rb
@@ -1,4 +1,8 @@
 class ParkingSpaceController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create,]
+
+
   def index
     @parking_spaces = ParkingSpace.all
   end
@@ -9,18 +13,19 @@ class ParkingSpaceController < ApplicationController
   end
 
   def create
-    @parking_space = ParkingSpace.new
+    @parking_space = ParkingSpace.new(parking_space_params)
     @user = User.find(params[:user_id])
     if @parking_space.save
       redirect_to user_path(@user)
     else
       render new
+    end
   end
 
   private
 
   def parking_space_params
-    params.require(:parking_space).permit(:)
+    params.require(:parking_space).permit(:adress, :postcode, :city, :country, :latitude, :longitude, :type, :availability, :price_per_hour)
   end
 
 end
