7b36343e955a150f80b4c4fb24fdda89c8bc69c4
Linnie <38510016+linniethepooh@users.noreply.github.com>
2018-08-01 15:57:50 +0800

    Update 20180801074137_create_bookings.rb

--- db/migrate/20180801074137_create_bookings.rb
+++ db/migrate/20180801074137_create_bookings.rb
@@ -5,7 +5,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
       t.references :car, foreign_key: true
       t.datetime :start_time
       t.datetime :end_time
-      t.integer :prince
+      t.integer :price
       t.string :status
 
       t.timestamps
