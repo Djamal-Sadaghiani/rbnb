2828758e98287f744077e2e2ba62e3e6e7bf7717
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-01 16:36:18 +0800

    updated routes + beginning of parking sapce controller and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_064118) do
+ActiveRecord::Schema.define(version: 2018_08_01_074137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "parking_space_id"
+    t.bigint "car_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer "price"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["parking_space_id"], name: "index_bookings_on_parking_space_id"
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "license"
+    t.string "brand"
+    t.string "model"
+    t.string "colour"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
 
   create_table "parking_spaces", force: :cascade do |t|
     t.float "size"
@@ -68,5 +92,8 @@ ActiveRecord::Schema.define(version: 2018_08_01_064118) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "parking_spaces"
+  add_foreign_key "cars", "users"
   add_foreign_key "parking_spaces", "users"
 end
