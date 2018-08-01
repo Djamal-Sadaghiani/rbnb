b4b71f9cef96a8932c0664fc3b84f59da220be39
Djamal Sadaghiani <dj8amal@gmail.com>
2018-08-01 14:49:00 +0800

    Added Ralationships to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  #Relationships
+  has_many :parking_spaces
+  has_many :cars
+  has_many :bookings, through: :cars
+  has_many :bookings, through: :parking_spaces
+
+  #Validations
+
+end
 end
