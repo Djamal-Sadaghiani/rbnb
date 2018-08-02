f54af9b841a888a538a77979e5450bf7c3fcf97d
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-02 12:46:07 +0800

    finish bookings controller and added one route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,41 +1,71 @@
 class BookingsController < ApplicationController
 
-  def new
-    @user = current_user
-    @cars = @user.cars
-    @licenses = []
-    @cars.each do |car|
-      @licenses << car.license
+  before_action :set_booking, only: [:show, :update, :edit]
+  before_action :set_parking_space, only: [:new, :create, :edit]
+  before_action :set_licenses, only: [:new, :create, :edit, :update]
+
+  def index
+    user = current_user
+    cars = user.cars
+    cars_id = []
+    cars.each do |car|
+      cars_id << car.id
     end
-    @parking_space = ParkingSpace.find(params[:parking_space_id])
+    @bookings = Booking.where(car_id: cars_id)
+  end
+
+  def new
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.price = 2
-    @booking.parking_space = ParkingSpace.find(params[:parking_space_id])
     @booking.car = Car.find_by(license: params[:license])
+    @booking.parking_space = @parking_space
     if @booking.save
-      redirect_to @booking
+      redirect_to parking_space_booking_path(@booking.parking_space, @booking)
     else
       render :new
     end
   end
 
   def show
+    @parking_space = ParkingSpace.find_by(id: @booking.parking_space_id)
+    @car = Car.find_by(id: @booking.car_id)
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.parking_space = ParkingSpace.find(params[:parking_space_id])
+    @booking.car = Car.find_by(license: params[:license])
+    if @booking.update(booking_params)
+      redirect_to parking_space_booking_path(@booking.parking_space, @booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_booking
     @booking = Booking.find(params[:id])
   end
 
-  # def edit
-  #   #render
-  # end
+  def set_parking_space
+    @parking_space = ParkingSpace.find(params[:parking_space_id])
+  end
 
-  # def update
-  #   @booking(booking_params)
-  # end
-
-  private
+  def set_licenses
+    @user = current_user
+    @cars = @user.cars
+    @licenses = []
+    @cars.each do |car|
+      @licenses << car.license
+    end
+  end
 
   def booking_params
     params.require(:booking).permit(:start_time, :end_time, :price, :status)
