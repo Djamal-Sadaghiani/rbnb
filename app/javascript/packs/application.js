03ba4e369acd2935c5e44a20003aa9a6f5359504
Anton Barthelmess <anton.barthelmess@gmail.com>
2018-08-02 17:49:45 +0800

    formatted date in booking form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
+import "./application.css";
 import "bootstrap";
+import { toggleDatepicker } from "../components/booking_form"
+
+
+
+const parkingSpaceShow = document.querySelector('.parking_spaces.show');
+if (parkingSpaceShow) {
+  toggleDatepicker();
+}
