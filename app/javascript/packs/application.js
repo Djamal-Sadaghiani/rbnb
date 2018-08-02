f0e046207bf30c0c8543a8f35a646a52154927d9
Djamal Sadaghiani <dj8amal@gmail.com>
2018-08-02 18:06:05 +0800

    Added the static Map to the show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,10 @@
 import "./application.css";
 import "bootstrap";
-import GMaps from 'gmaps';
 import { toggleDatepicker } from "../components/booking_form"
+import { showMap } from "../components/maps"
 
 const parkingSpaceShow = document.querySelector('.parking_spaces.show');
 if (parkingSpaceShow) {
   toggleDatepicker();
+  showMap();
 }
--- app/javascript/packs/maps.js
+++
@@ -1,4 +0,0 @@
-const map = new GMaps({ el: '#map_show', lat: 48.8648482, lng: 2.3798534, zoom: 14 });
-const marker = { lat: 48.8648482, lng: 2.3798534 };
-map.addMarkers([ marker ]);
-console.log("HI")
